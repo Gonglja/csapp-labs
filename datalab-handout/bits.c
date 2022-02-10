d69ec88bcf25271034d215a72e9a68b656f7b487
gonglja <glj0@outlook.com>
2022-02-10 18:58:11 +0800

    update:datalab-handout

--- datalab-handout/bits.c
+++ datalab-handout/bits.c
@@ -143,7 +143,7 @@ NOTES:
  *   Rating: 1
  */
 int bitXor(int x, int y) {
-  return 2;
+    return ~(~(x&~y) & ~(~x&y));
 }
 /* 
  * tmin - return minimum two's complement integer 
@@ -153,7 +153,7 @@ int bitXor(int x, int y) {
  */
 int tmin(void) {
 
-  return 2;
+  return 1 << 31;
 
 }
 //2
@@ -165,7 +165,7 @@ int tmin(void) {
  *   Rating: 1
  */
 int isTmax(int x) {
-  return 2;
+    return 2;
 }
 /* 
  * allOddBits - return 1 if all odd-numbered bits in word set to 1
@@ -176,7 +176,11 @@ int isTmax(int x) {
  *   Rating: 2
  */
 int allOddBits(int x) {
-  return 2;
+    int a = 0xaa;
+    int b = a | (a << 8 );
+    int c = b | (b << 16);
+
+    return !((x & c) ^ c);
 }
 /* 
  * negate - return -x 
@@ -186,7 +190,8 @@ int allOddBits(int x) {
  *   Rating: 2
  */
 int negate(int x) {
-  return 2;
+
+  return ~x + 1;
 }
 //3
 /* 
@@ -199,7 +204,13 @@ int negate(int x) {
  *   Rating: 3
  */
 int isAsciiDigit(int x) {
-  return 2;
+  /* x >= 0x30  --> x - 0x30 >=0 */
+  /* x <= 0x39  --> 0x39 - x >=0 */
+  int res1 = x + ~0x30 + 1;
+  int res2 = 0x39 + ~x + 1;
+  int sign1 = res1 >> 31 & 1;
+  int sign2 = res2 >> 31 & 1;
+  return (!sign1 & !sign2);
 }
 /* 
  * conditional - same as x ? y : z 
@@ -209,7 +220,9 @@ int isAsciiDigit(int x) {
  *   Rating: 3
  */
 int conditional(int x, int y, int z) {
-  return 2;
+    x = !!x;
+    x = ~x + 1;
+    return (x & y) | (~x & z);
 }
 /* 
  * isLessOrEqual - if x <= y  then return 1, else return 0 
@@ -219,7 +232,13 @@ int conditional(int x, int y, int z) {
  *   Rating: 3
  */
 int isLessOrEqual(int x, int y) {
-  return 2;
+    int signX = x >> 31 & 1;
+    int signY = y >> 31 & 1;
+    /* x <= y --> y - x >=0 return 1;else 0*/
+    int ySubX = y + ~x + 1;
+    int signYSubX = ySubX >> 31 & 1;
+    int xor = signX ^ signY;
+    return (xor & !signY) | (!xor & !signYSubX);
 }
 //4
 /* 
@@ -231,7 +250,9 @@ int isLessOrEqual(int x, int y) {
  *   Rating: 4 
  */
 int logicalNeg(int x) {
-  return 2;
+    int signX = x >> 31 & 1;
+    int signSubX = (~x + 1) >> 31 & 1;
+  return (!signX & !signSubX);
 }
 /* howManyBits - return the minimum number of bits required to represent x in
  *             two's complement
@@ -246,7 +267,22 @@ int logicalNeg(int x) {
  *  Rating: 4
  */
 int howManyBits(int x) {
-  return 0;
+    int b16,b8,b4,b2,b1,b0;
+    int signX = x >> 31;
+    x = (signX & ~x) | (~signX & x);
+
+    b16 = !!(x >> 16) << 4;
+    x >>= b16;
+    b8  = !!(x >> 8 ) << 3;
+    x >>= b8;
+    b4  = !!(x >> 4 ) << 2;
+    x >>= b4;
+    b2  = !!(x >> 2 ) << 1;
+    x >>= b2;
+    b1  = !!(x >> 1 ) << 0;
+    x >>= b1;
+    b0  = !!(x >> 0 );
+    return b16 + b8 + b4 + b2 + b1 + b0 + 1;
 }
 //float
 /* 
